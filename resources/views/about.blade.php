5b244f687d35d643be57a9c4de1c47803bc485c3
shoffistudy <sholikhahshoffifus@gmail.com>
2024-07-15 14:15:08 +0700

    membuat halaman about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!-- resources/views/about.blade.php -->
+
+{{-- <!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <p>Ini adalah halaman About.</p>
+</body>
+</html> --}}
+
+
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>About Us</h1>
+        <p>Selamat datang di halaman About kami. Di sini, Anda dapat mengetahui lebih banyak tentang kami.</p>
+        <p>Konten lainnya bisa ditambahkan di sini.</p>
+    </div>
+@endsection
---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white" style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: transparent; opacity: 0.8; padding: 10px 0;">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright @2024 - PT INKA Multi Solusi</span>
+        </div>
+    </div>
+</footer>
